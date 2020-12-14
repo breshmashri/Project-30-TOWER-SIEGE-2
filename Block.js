3aa26f01e457802186038f9c47dd0217286a60f9
breshmashri <73825231+breshmashri@users.noreply.github.com>
2020-12-14 09:30:32 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -13,14 +13,15 @@ class Block {
   }
   display() {
       console.log(this.body.speed);
-      if (this.body.speed < 3) {
+      if (this.body.speed < 10) {
             var angle = this.body.angle;
             var pos = this.body.position;
             push()
             translate(pos.x,pos.y);
+            //translate(0,0,this.width,this.height);
             rotate(angle);
             rectMode(CENTER);
-            rect(50,50,this.width,this.height);
+            rect(0,0,this.width,this.height);
             pop()
       } 
       else {
