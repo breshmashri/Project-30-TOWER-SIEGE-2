5c6888371de17331e1536e6a4f5c2c231bde7fc6
breshmashri <73825231+breshmashri@users.noreply.github.com>
2020-12-17 21:04:03 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -1,9 +1,9 @@
 class Block {
   constructor(x,y,width,height) {
       var options = {
-          restitution:0.4,
-          friction:0.0,
-          //isStatic:false
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
       }
       this.visiblity = 255;
       this.body = Bodies.rectangle(x,y,width,height,options);
@@ -12,12 +12,10 @@ class Block {
       World.add(world,this.body);
   }
   display() {
-      console.log(this.body.speed);
       if (this.body.speed < 10) {
             var angle = this.body.angle;
-            var pos = this.body.position;
             push()
-            translate(pos.x,pos.y);
+            translate(this.body.position.x,this.body.position.y);
             rotate(angle);
             rectMode(CENTER);
             rect(0,0,this.width,this.height);
